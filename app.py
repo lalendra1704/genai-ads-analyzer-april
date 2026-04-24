8164f0ac2a18db00f4711f06eb62301d2d1d38d5
lalendra1704 <lalendra1704@gmail.com>
2026-04-24 12:56:27 +0530

    Switched from Gemini to Groq LLaMA 3 model

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import streamlit as st
 
 import pandas as pd
 
+from groq import Groq
+import os
+
+client = Groq(api_key=st.secrets["GROQ_API_KEY"])
+
 st.title("📊 GenAI Google Ads Analyzer")
 
 st.write("Upload your Google Ads CSV file to analyze performance.")
